2db466c20b8f1413e5269ffda08a24dc8ef22f00
Thekho Ngaosathe <bennetonss@hotmail.com>
2016-05-02 08:55:43 +0530

    First commit react routes

---
+++ app/components/About.js
@@ -0,0 +1,18 @@
+var React = require('react');
+
+var About = React.createClass({
+  render: function(){
+    return (
+      <div>
+      <h2>About</h2>
+      <br/>
+        <p>
+          Thekho Ngaosathe is a Passionate and Extremely hard-working Person.
+          He loves MMA and also works on Front-end Projects.
+        </p>
+      </div>
+    )
+  }
+});
+
+module.exports = About;
---
+++ app/config/routes.js
@@ -0,0 +1,25 @@
+var React = require('react');
+var ReactRouter = require('react-router');
+
+//react-router options
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var IndexRoute = ReactRouter.IndexRoute;
+var hashHistory = ReactRouter.hashHistory;
+
+//
+var Main = require('../components/Main');
+var Home = require('../components/Home');
+var About = require('../components/About');
+
+
+var routes = (
+  <Router history={hashHistory}>
+    <Route path='/' component={Main}>
+      <IndexRoute component={Home}/>
+      <Route path='/about' component={About}/>
+    </Route>
+  </Router>
+);
+
+module.exports = routes;
